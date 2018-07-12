528b5ef38a917db8a02db34e48ded7c0907e25f5
Hammathu Muneerul Huda <Hammathu@qatalystechnologies.co.in>
2018-07-12 15:47:22 +0530

    sqlCmd ExecuteReader in GetSchema method modified

--- CodeGenerator.cs
+++ CodeGenerator.cs
@@ -69,8 +69,8 @@ namespace SpParamClassGenerater
                     clsmName = clsmName.Replace("[", "");
                     clsmName = clsmName.Replace("]", "");
                     //clsmName = clsmName.Replace(".", "");
-                    if(clsmName.Contains('.'))
-                    clsmName = clsmName.Split('.')[1];
+                    if (clsmName.Contains('.'))
+                        clsmName = clsmName.Split('.')[1];
                 }
 
                 var Params = GetSPDetails(sp.Spname, conStr)?.Parameters;
@@ -343,8 +343,8 @@ namespace SpParamClassGenerater
                 //storedProcedureText = "SET FMTONLY ON;" + storedProcedureText;
                 SqlCommand sqlCmd = new SqlCommand(storedProcedureText, sqlCon);
                 sqlCon.Open();
-                SqlDataReader sqlReader = sqlCmd.ExecuteReader(CommandBehavior.SchemaOnly);
-                //SqlDataReader sqlReader = sqlCmd.ExecuteReader();
+              //  SqlDataReader sqlReader = sqlCmd.ExecuteReader(CommandBehavior.SchemaOnly);
+                SqlDataReader sqlReader = sqlCmd.ExecuteReader();
                 while (sqlReader.FieldCount != 0)
                 {
                     i++;
@@ -469,7 +469,77 @@ namespace SpParamClassGenerater
             return Sps;
         }
 
+        //-- Table to class
+        public string ConvertTableToClass(string tblName)
+        {
+            using (SqlConnection con = new SqlConnection(Home.Constr))
+            {
+                string qry = @"declare @TableName sysname = '-TBL-'
+declare @Result varchar(max) = 'public class ' + @TableName + '
+{'
 
+select @Result = @Result + '
+    public ' + ColumnType + NullableSign + ' ' + ColumnName + ' { get; set; }
+'
+from
+(
+    select 
+        replace(col.name, ' ', '_') ColumnName,
+        column_id ColumnId,
+        case typ.name 
+            when 'bigint' then 'long'
+            when 'binary' then 'byte[]'
+            when 'bit' then 'bool'
+            when 'char' then 'string'
+            when 'date' then 'DateTime'
+            when 'datetime' then 'DateTime'
+            when 'datetime2' then 'DateTime'
+            when 'datetimeoffset' then 'DateTimeOffset'
+            when 'decimal' then 'decimal'
+            when 'float' then 'float'
+            when 'image' then 'byte[]'
+            when 'int' then 'int'
+            when 'money' then 'decimal'
+            when 'nchar' then 'string'
+            when 'ntext' then 'string'
+            when 'numeric' then 'decimal'
+            when 'nvarchar' then 'string'
+            when 'real' then 'double'
+            when 'smalldatetime' then 'DateTime'
+            when 'smallint' then 'short'
+            when 'smallmoney' then 'decimal'
+            when 'text' then 'string'
+            when 'time' then 'TimeSpan'
+            when 'timestamp' then 'DateTime'
+            when 'tinyint' then 'Byte'
+            when 'uniqueidentifier' then 'Guid'
+            when 'varbinary' then 'byte[]'
+            when 'varchar' then 'string'
+            else 'UNKNOWN_' + typ.name
+        end ColumnType,
+        case 
+            when col.is_nullable = 1 and typ.name in ('bigint', 'bit', 'date', 'datetime', 'datetime2', 'datetimeoffset', 'decimal', 'float', 'int', 'money', 'numeric', 'real', 'smalldatetime', 'smallint', 'smallmoney', 'time', 'tinyint', 'uniqueidentifier') 
+            then '?' 
+            else '' 
+        end NullableSign
+    from sys.columns col
+        join sys.types typ on
+            col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
+    where object_id = object_id(@TableName)
+) t
+order by ColumnId
+
+set @Result = @Result  + '
+}'
+Select @Result";
+                qry = qry.Replace("-TBL-", tblName);
+                SqlCommand cmd = new SqlCommand(qry, con);
+                con.Open();
+                string result = cmd.ExecuteScalar().ToString();
+                con.Close();
+                return result;
+            }
+        }
     }
 
 
